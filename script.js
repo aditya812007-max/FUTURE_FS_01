60db2375dccf90fc709cd930e46e29f5d38e6f27
Aditya <aditya.student@example.com>
2025-12-11 18:20:15 +0530

    Day 4 update: Added Glassmorphism and Background Image

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 const toggleButton = document.getElementById('theme-toggle');
 const body = document.body;
 
-toggleButton.addEventListener('click', function() {
+toggleButton.addEventListener('click',function() {
     body.classList.toggle('dark-mode');
-
-    // Change icon
     if (body.classList.contains('dark-mode')) {
-        toggleButton.textContent = '☀️ Light Mode';
-    } else {
-        toggleButton.textContent = '🌙 Dark Mode';
+        toggleButton.textContent = "☀️ Light Mode";
+    }else {
+        toggleButton.textContent = "🌙 Dark Mode";
     }
-});
+    });
