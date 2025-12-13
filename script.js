14be3c7329b2507cb41ca24acc1fff266c7654b6
Aditya <aditya.student@example.com>
2025-12-13 16:47:09 +0530

    Day 6: Added dynamic project rendering

--- script.js
+++ script.js
@@ -9,3 +9,34 @@ toggleButton.addEventListener('click',function() {
         toggleButton.textContent = "🌙 Dark Mode";
     }
     });
+
+    const projects = [
+    {
+        title: "Portfolio Website",
+        description: "My personal digital garden built with HTML, CSS, and JS.",
+        link: "#"
+    },
+    {
+        title: "Dark Mode Feature",
+        description: "A logic-based toggle switch to change the theme for better UX.",
+        link: "#"
+    },
+    {
+        title: "Glassmorphism UI",
+        description: "A glassmorphism UI element with mobile responsiveness.",
+        link: "#"
+    }
+];
+
+console.log(projects); 
+const projectContainer = document.getElementById("projects-container");
+projects.forEach(function(project) {
+    
+    projectContainer.innerHTML += `
+        <div class="project-card">
+            <h3>${project.title}</h3>
+            <p>${project.description}</p>
+            <a href="${project.link}"><button>View Project</button></a>
+        </div>
+    `;
+});
