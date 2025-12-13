e82a5334b168013b0f2c12ac3bb6ff980f4955a5
Aditya <aditya.student@example.com>
2025-12-13 23:38:20 +0530

    Day 6 update : removed view project button

--- script.js
+++ script.js
@@ -36,7 +36,6 @@ projects.forEach(function(project) {
         <div class="project-card">
             <h3>${project.title}</h3>
             <p>${project.description}</p>
-            <a href="${project.link}"><button>View Project</button></a>
         </div>
     `;
 });
